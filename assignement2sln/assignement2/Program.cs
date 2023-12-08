8761561c0f28977eac359aa3c1822e8593ffc07d
tylintheslayer <151826095+tylintheslayer@users.noreply.github.com>
2023-12-08 14:36:13 +0200

    first commit

---
+++ assignement2sln/assignement2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace assignement2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+            {
+                Console.WriteLine("Enter a four-digit number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 0 || number > 9999)
+                {
+                    Console.WriteLine("Please enter a valid four-digit number.");
+                }
+                else if (number == 0)
+                {
+                    Console.WriteLine("Zero");
+                }
+                else
+                {
+                    Console.WriteLine(numberstowords.ConvertToWords(number));
+                }
+
+            }
+        }
+    }
+
+}
+    
---
+++ assignement2sln/assignement2/numberstowords.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignement2
+{
+    internal class numberstowords
+    {
+        static string[] units = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+        static string[] teens = { "", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+        static string[] tens = { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+
+        public static string ConvertToWords(int number)
+        {
+            if (number >= 1000)
+            {
+                return $"{units[number / 1000]} Thousand {ConvertToWords(number % 1000)}";
+            }
+            else if (number >= 100)
+            {
+                return $"{units[number / 100]} Hundreds {ConvertToWords(number % 100)}";
+            }
+            else if (number >= 20)
+            {
+                return $"{tens[number / 10]} {units[number % 10]}";
+
+            }
+            else if (number >= 11)
+            {
+                return $"{teens[number - 10]}";
+            }
+            else
+            {
+                return $"{units[number]}";
+
+            }
+
+        }
+
+    }
+}
+    
+
